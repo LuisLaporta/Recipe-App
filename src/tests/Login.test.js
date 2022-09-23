0114f5756091b8e65b901ca407cb194e0b85fcf5
Lorena Mendes <lorena.lccma@gmail.com>
2022-09-23 20:26:13 -0300

    Requisitos de 1 a 6 finalizados

---
+++ src/tests/Login.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './Helpers/RenderWithRouter';
+
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
+
+describe('Testando tela de Login', () => {
+  test('Verifica se os inputs estão na tela', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByTestId(EMAIL_INPUT)).toBeInTheDocument();
+    expect(screen.getByTestId(PASSWORD_INPUT)).toBeInTheDocument();
+  });
+
+  test('Verifica se o botão está na tela', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('button', { name: /enter/i })).toBeInTheDocument();
+  });
+
+  test('Verifica se o botão está desabilitado ao entrar na tela', () => {
+    renderWithRouter(<App />);
+    const buttonSubmit = screen.getByRole('button', { name: /enter/i });
+    expect(buttonSubmit).toBeDisabled();
+  });
+
+  test('Verifica se o email correto e a senha incorreta o botão continua desabilitado', () => {
+    renderWithRouter(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const buttonSubmit = screen.getByRole('button', { name: /enter/i });
+
+    userEvent.type(inputEmail, 'teste@teste.com');
+    userEvent.type(inputPassword, '1234');
+    expect(buttonSubmit).toBeDisabled();
+  });
+
+  test('Verifica se o email incorreto e a senha correta o botão continua desabilitado', () => {
+    renderWithRouter(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const buttonSubmit = screen.getByRole('button', { name: /enter/i });
+
+    userEvent.type(inputEmail, 'teste');
+    userEvent.type(inputPassword, '123456');
+    expect(buttonSubmit).toBeDisabled();
+  });
+
+  test('Verifica se o email e a senha corretas o botão é habilitado', () => {
+    renderWithRouter(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const buttonSubmit = screen.getByRole('button', { name: /enter/i });
+
+    userEvent.type(inputEmail, 'teste@teste.com');
+    userEvent.type(inputPassword, '1234567');
+    expect(buttonSubmit).not.toBeDisabled();
+  });
+
+  // test('Verifica se o email e a senha corretas o botão é habilitado', async () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   const inputEmail = screen.getByTestId('EMAIL_INPUT');
+  //   const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+  //   const buttonSubmit = screen.getByRole('button', { name: /enter/i });
+
+  //   userEvent.type(inputEmail, 'teste@teste.com');
+  //   userEvent.type(inputPassword, '1234567');
+  //   userEvent.click(buttonSubmit);
+
+  //   await waitFor(() => {
+  //     expect(history.location.pathname).toBe('/meals');
+  //   }, { timeout: 3000 });
+  // });
+});
