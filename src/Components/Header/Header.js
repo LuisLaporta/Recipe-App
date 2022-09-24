5b4d6e8726986d32061598ad70532ae9619cfb26
Luis Laporta <lui.laporta99@gmail.com>
2022-09-24 00:41:36 -0300

    Requisito 9 - Implementação de lógica para renderização da searchBar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import SearchBar from '../SearchBar/SearchBar';
 // import profileIcon from './images/profileIcon.svg';
 // import searchIcon from './images/searchIcon.svg';
 
 function Header({ title, disabledSearch }) {
+  const [searchBar, setSearchBar] = useState(false);
+
   const history = useHistory();
   return (
     <div>
@@ -16,14 +19,15 @@ function Header({ title, disabledSearch }) {
         />
       </button>
       {disabledSearch && (
-        <button type="button">
+        <button type="button" onClick={ () => setSearchBar(!searchBar) }>
           <img
             src="./images/searchIcon.svg"
-            alt="Profile Icon"
+            alt="Search Icon"
             data-testid="search-top-btn"
           />
         </button>
       )}
+      {searchBar && <SearchBar />}
       <h1 data-testid="page-title">{title}</h1>
     </div>
   );
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="search" placeholder="Search" data-testid="search-input" />
+    </div>
+  );
+}
+
+export default SearchBar;
