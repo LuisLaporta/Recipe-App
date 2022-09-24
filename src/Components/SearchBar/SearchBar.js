74858324e135b2272c883dbb7c9e2d1daaf4384d
Luis Laporta <lui.laporta99@gmail.com>
2022-09-24 11:30:43 -0300

    Requisito 10 - Implementação dos inputs de radios e do botão de busca

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,6 +4,36 @@ function SearchBar() {
   return (
     <div>
       <input type="search" placeholder="Search" data-testid="search-input" />
+      <div>
+        <label htmlFor="ingredient">
+          Ingredient
+          <input
+            type="radio"
+            name="ingredient"
+            value="Ingredient"
+            data-testid="ingredient-search-radio"
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="radio"
+            name="name"
+            value="Name"
+            data-testid="name-search-radio"
+          />
+        </label>
+        <label htmlFor="firstLetter">
+          First Letter
+          <input
+            type="radio"
+            name="firstLetter"
+            value="First Letter"
+            data-testid="first-letter-search-radio"
+          />
+        </label>
+      </div>
+      <button type="button" data-testid="exec-search-btn">Buscar</button>
     </div>
   );
 }
