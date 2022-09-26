6af27ce91f4dd097173862fd5f78366ce2dcc774
Lorena Mendes <lorena.lccma@gmail.com>
2022-09-26 15:58:25 -0300

    Requisitos 58 e 59

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
+// import Login from '../Login/Login';
 
 function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  // console.log(user);
+
   return (
     <div>
       <Header title="Profile" disabledSearch={ false } />
       <Footer />
       <div>
-        <h3 data-testid="profile-email">
+        <p data-testid="profile-email">
           E-mail:
-        </h3>
+          {user && user.email}
+        </p>
         <button
           type="button"
           data-testid="profile-done-btn"
