33bff0086d905667ba7f187f12ab99429a43eabb
LuisLaporta <lui.laporta99@gmail.com>
2024-02-05 13:38:28 -0300

    feat: estilização do recipes

--- src/Components/Recipes/Recipes.js
+++ src/Components/Recipes/Recipes.js
@@ -1,12 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
+import PropTypes from 'prop-types';
 
 import RecipesContext from '../../Context/RecipesContext';
+import '../../css/recipes.css';
 
 const LIMIT_NUMBER = 12;
 
-function Recipes() {
+function Recipes({ searchBarVisible }) {
   const { searchedRecipe, categorySelected } = useContext(RecipesContext);
 
   const [recipesFound, setRecipesFound] = useState([]);
@@ -27,29 +29,38 @@ function Recipes() {
   );
 
   return (
-    <div>
+    <div
+      className={
+        searchBarVisible ? 'drop-down  flex-recipe'
+          : 'recipes-container flex-recipe'
+      }
+    >
       { recipesFound?.map((recipe, index) => (
         <div
           key={ index }
           role="presentation"
           data-testid={ `${index}-recipe-card` }
+          className="recipe-card"
           onClick={ () => goToDetails(pathname, recipe) }
         >
           <img
             src={ pathname === '/meals' ? recipe.strMealThumb : recipe.strDrinkThumb }
             alt={ pathname === '/meals' ? recipe.strMeal : recipe.strDrink }
             data-testid={ `${index}-card-img` }
+            className="img-recipe"
           />
-          <h3 data-testid={ `${index}-card-name` }>
+          <h3 data-testid={ `${index}-card-name` } className="recipe-name">
             {history.location.pathname === '/meals' ? recipe.strMeal : recipe.strDrink}
           </h3>
-          <h4 data-testid={ `${index}-card-category` }>
-            { categorySelected === 'all' ? recipe.strCategory : categorySelected }
-          </h4>
         </div>
       ))}
+      <div className="free" />
     </div>
   );
 }
+
+Recipes.propTypes = {
+  searchBarVisible: PropTypes.bool.isRequired,
+};
 
 export default Recipes;
