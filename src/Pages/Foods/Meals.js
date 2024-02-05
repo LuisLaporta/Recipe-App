9b41dd890f9208291f27712af56f34d37722ac64
LuisLaporta <lui.laporta99@gmail.com>
2024-02-05 13:40:39 -0300

    feat: estilização de food

--- src/Pages/Foods/Meals.js
+++ src/Pages/Foods/Meals.js
@@ -1,31 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
-// import requestRecipesApi from '../../Services/RequestRecipesApi';
+import SearchBar from '../../Components/SearchBar/SearchBar';
 import RecipesContext from '../../Context/RecipesContext';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 import Recipes from '../../Components/Recipes/Recipes';
 import FilterButtons from '../../Components/FilterButtons/FilterButtons';
 import useRequestApi from '../../Hooks/useRequestApi';
+import '../../css/food.css';
 
 function Meals() {
   const { updateRecipeList } = useContext(RecipesContext);
 
-  // useEffect(() => {
-  //   const fetchRecipes = async () => {
-  //     const data = await requestRecipesApi('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-  //     updateRecipeList(data.meals);
-  //   };
-  //   fetchRecipes();
-  // }, []);
+  const [searchBarVisible, setSearchBarVisible] = useState(false);
+
+  const handleSearchIconClick = () => {
+    setSearchBarVisible(!searchBarVisible);
+  };
 
   useRequestApi('/meals', 'all', updateRecipeList);
-
+  console.log(useRequestApi('/meals', 'all', updateRecipeList));
   return (
-    <div>
-      <Header title="Meals" disabledSearch />
-      <FilterButtons />
-      <Recipes />
+    <div className={ `app-container ${searchBarVisible ? 'search-bar-visible' : ''}` }>
+      <Header title="Meals" disabledSearch={ handleSearchIconClick } disabled />
+      {searchBarVisible && <SearchBar />}
+      <FilterButtons searchBarVisible={ searchBarVisible } />
+      <Recipes searchBarVisible={ searchBarVisible } />
       <Footer />
     </div>
   );
