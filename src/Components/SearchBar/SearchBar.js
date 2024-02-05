7cfa62b023296f018a1284d7dfb3b5348db01d75
LuisLaporta <lui.laporta99@gmail.com>
2024-02-05 13:39:49 -0300

    feat: estilização do search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipesContext from '../../Context/RecipesContext';
 import getRecipe from '../../Services/GetRecipesApi';
+import '../../css/searchBar.css';
 
 function SearchBar() {
   const { setSearchedRecipe, searchedRecipe } = useContext(RecipesContext);
@@ -27,55 +28,61 @@ function SearchBar() {
   };
 
   return (
-    <div>
+    <div className="searchBar-container">
       <input
         type="search"
         placeholder="Search"
         data-testid="search-input"
         onChange={ ({ target: { value } }) => setInputsearch(value) }
+        className="search-input"
       />
-      <div>
-        <label htmlFor="ingredient">
-          <input
-            id="ingredient"
-            type="radio"
-            name="radio-search"
-            value="Ingredient"
-            data-testid="ingredient-search-radio"
-            onChange={ ({ target: { value } }) => setTypeRadio(value) }
-          />
-          Ingredient
-        </label>
-        <label htmlFor="name">
-          <input
-            id="name"
-            type="radio"
-            name="radio-search"
-            value="Name"
-            data-testid="name-search-radio"
-            onChange={ ({ target: { value } }) => setTypeRadio(value) }
-          />
-          Name
-        </label>
-        <label htmlFor="firstLetter">
-          <input
-            id="firstLetter"
-            type="radio"
-            name="radio-search"
-            value="First Letter"
-            data-testid="first-letter-search-radio"
-            onChange={ ({ target: { value } }) => setTypeRadio(value) }
-          />
-          First Letter
-        </label>
+      <div className="filter-search-container">
+        <div className="flex-filter">
+          <label htmlFor="ingredient" className="ingredient-search-radio">
+            <input
+              id="ingredient"
+              type="radio"
+              name="radio-search"
+              value="Ingredient"
+              data-testid="ingredient-search-radio"
+              onChange={ ({ target: { value } }) => setTypeRadio(value) }
+            />
+            <p className="p-cont">Ingredient</p>
+          </label>
+          <label htmlFor="name">
+            <input
+              id="name"
+              type="radio"
+              name="radio-search"
+              value="Name"
+              data-testid="name-search-radio"
+              onChange={ ({ target: { value } }) => setTypeRadio(value) }
+              className="name-search-radio"
+            />
+            <p className="p-cont">Name</p>
+          </label>
+          <label htmlFor="firstLetter">
+            <input
+              id="firstLetter"
+              type="radio"
+              name="radio-search"
+              value="First Letter"
+              data-testid="first-letter-search-radio"
+              onChange={ ({ target: { value } }) => setTypeRadio(value) }
+              className="first-letter-search-radio"
+            />
+            <p className="p-cont">First Letter</p>
+          </label>
+        </div>
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="exec-search-btn"
+          className="btn-search"
+        >
+          Buscar
+        </button>
       </div>
-      <button
-        type="button"
-        onClick={ handleClick }
-        data-testid="exec-search-btn"
-      >
-        Buscar
-      </button>
     </div>
   );
 }
