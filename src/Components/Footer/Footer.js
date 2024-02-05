d493496440d9e71d8659d244fa12051dc9e0459b
LuisLaporta <lui.laporta99@gmail.com>
2024-02-05 13:38:07 -0300

    feat: estilização do footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import drinkIcon from '../../images/drinkIcon.svg';
-import mealIcon from '../../images/mealIcon.svg';
-import './Footer.css';
+import drinkIcon from '../../images/drinksIcon.svg';
+import mealIcon from '../../images/mealsIcon.svg';
+import '../../css/Footer.css';
 
 function Footer() {
   const history = useHistory();
   return (
-    <footer data-testid="footer">
+    <footer data-testid="footer" className="footer-container">
       <button type="button" onClick={ () => history.push('/drinks') }>
-        <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
+        <img
+          src={ drinkIcon }
+          alt="Drink Icon"
+          data-testid="drinks-bottom-btn"
+          className="drinkIcon"
+        />
       </button>
       <button type="button" onClick={ () => history.push('/meals') }>
-        <img src={ mealIcon } alt="Meal Icon" data-testid="meals-bottom-btn" />
+        <img
+          src={ mealIcon }
+          alt="Meal Icon"
+          data-testid="meals-bottom-btn"
+          className="mealIcon"
+        />
       </button>
     </footer>
   );
