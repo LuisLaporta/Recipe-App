66d81e812c591d3049ec0db14c2611cb406eb857
LuisLaporta <lui.laporta99@gmail.com>
2024-02-05 13:41:47 -0300

    feat: estilização do filter

--- src/Components/FilterButtons/FilterButtons.js
+++ src/Components/FilterButtons/FilterButtons.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
+import PropTypes from 'prop-types';
 
 import RecipesContext from '../../Context/RecipesContext';
 import requestRecipesApi from '../../Services/RequestRecipesApi';
 import useRequestApi from '../../Hooks/useRequestApi';
+import getIcon from '../../Services/GetIcon';
+import '../../css/filter.css';
 
 const MAX_CATEGORY_LENGTH = 5;
 
-function FilterButtons() {
+function FilterButtons({ searchBarVisible }) {
   const [filterButtons, setFilterButtons] = useState({
     allCategories: [],
     selectedCategories: [],
   });
   const [filterButton, setFilterButton] = useState('all');
   const { pathname } = useLocation();
+  const [loading, setLoading] = useState(true);
+  const [icon, setIcon] = useState({});
 
   const { updateRecipeList, changeCategory } = useContext(RecipesContext);
 
@@ -27,6 +32,8 @@ function FilterButtons() {
       );
       act(() => {
         setFilterButtons({ ...filterButtons, allCategories: data.drinks || data.meals });
+        setIcon(getIcon(pathname));
+        setLoading(false);
       });
     };
     fetchFilterButtons();
@@ -42,39 +49,54 @@ function FilterButtons() {
   useRequestApi(pathname, filterButton, updateRecipeList, filterButton);
 
   return (
-    <section>
-      <button
-        type="button"
-        data-testid="All-category-filter"
-        onClick={ () => {
-          setFilterButton('all');
-          changeCategory('all');
-        } }
-      >
-        All
-      </button>
-      {filterButtons?.selectedCategories.map((category, index) => (
-        <button
-          key={ index }
-          type="button"
-          data-testid={ `${category.strCategory}-category-filter` }
-          onClick={ () => (
-            filterButton === category.strCategory
-              ? act(() => {
+    <div>
+      {loading ? <div /> : (
+        <section
+          className={ searchBarVisible ? 'drop-down flex' : 'filter-container flex' }
+        >
+          <div>
+            <button
+              type="button"
+              data-testid="All-category-filter"
+              onClick={ () => {
                 setFilterButton('all');
                 changeCategory('all');
-              })
-              : act(() => {
-                setFilterButton(category.strCategory);
-                changeCategory(category.strCategory);
-              })
-          ) }
-        >
-          {category.strCategory}
-        </button>
-      ))}
-    </section>
+              } }
+            >
+              <img src={ icon[0] } alt="All" />
+            </button>
+            <p>All</p>
+          </div>
+          {filterButtons?.selectedCategories.map((category, index) => (
+            <div key={ index }>
+              <button
+                type="button"
+                data-testid={ `${category.strCategory}-category-filter` }
+                onClick={ () => (
+                  filterButton === category.strCategory
+                    ? act(() => {
+                      setFilterButton('all');
+                      changeCategory('all');
+                    })
+                    : act(() => {
+                      setFilterButton(category.strCategory);
+                      changeCategory(category.strCategory);
+                    })
+                ) }
+              >
+                <img src={ icon[category.strCategory] } alt={ category.strCategory } />
+              </button>
+              <p>{category.strCategory}</p>
+            </div>
+          ))}
+        </section>
+      )}
+    </div>
   );
 }
+
+FilterButtons.propTypes = {
+  searchBarVisible: PropTypes.bool.isRequired,
+};
 
 export default FilterButtons;
