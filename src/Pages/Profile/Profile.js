c2c2cda63173f71de7685074089cc0e691a41088
LuisLaporta <lui.laporta99@gmail.com>
2024-02-05 13:38:48 -0300

    feat: estilização do profile

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
+import doneIcon from '../../images/doneIcon.svg';
+import favIcon from '../../images/favIcon.svg';
+import logIcon from '../../images/logoutIcon.svg';
+import '../../css/profile.css';
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -28,31 +32,49 @@ function Profile() {
       <Header title="Profile" disabledSearch={ false } />
       <Footer />
       <div>
-        <p data-testid="profile-email">
-          E-mail:
+        <p data-testid="profile-email" className="profile-email">
           {user?.email}
         </p>
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-          onClick={ handleClickDoneRecipes }
-        >
-          Done Recipes
-        </button>
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-          onClick={ handleClickFavoriteRecipes }
-        >
-          Favorite Recipes
-        </button>
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ handleClickLogout }
-        >
-          Logout
-        </button>
+        <div className="btn-container">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            onClick={ handleClickDoneRecipes }
+            className="btn-profile profile-done-btn"
+          >
+            <img
+              src={ doneIcon }
+              alt="Done Icon"
+            />
+            Done Recipes
+          </button>
+          <hr className="line" />
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            onClick={ handleClickFavoriteRecipes }
+            className="btn-profile profile-favorite-btn"
+          >
+            <img
+              src={ favIcon }
+              alt="Favorite Icon"
+            />
+            Favorite Recipes
+          </button>
+          <hr className="line" />
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ handleClickLogout }
+            className="btn-profile profile-logout-btn"
+          >
+            <img
+              src={ logIcon }
+              alt="Logout Icon"
+            />
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
